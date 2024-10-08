06df0b63fa7ea6586fc7f0cc66c1e100f7665f45
lisa.todesco <todesco@gmail.com>
2024-10-08 18:53:32 +0800

    add project detail view

--- crowdfunding/projects/views.py
+++ crowdfunding/projects/views.py
@@ -1,5 +1,7 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework import status
+from django.http import Http404
 from .models import Project
 from .serializers import ProjectSerializer
 
@@ -12,5 +14,29 @@ class ProjectList(APIView):
         serializer = ProjectSerializer(projects, many=True)
         #return a response containing the serialized data
         return Response(serializer.data)
-    
+    def post(self, request):
+        serializer = ProjectSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(
+                serializer.data,
+                status=status.HTTP_201_CREATED
+            )
+        return Response(
+            serializer.errors,
+            status=status.HTTP_401_UNAUTHORIZED
+        )
+
+class ProjectDetail(APIView):
+    def get_object(self, pk):
+        try:
+            project = Project.objects.get(pk=pk)
+            return project
+        except Project.DoesNotExist:
+            raise Http404
+        
+    def get(self, request, pk):
+        project = self.get_object(pk)
+        serializer = ProjectSerializer(project)
+        return Response(serializer.data)
 
